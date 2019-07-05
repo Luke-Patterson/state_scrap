ab50c4e23bb1113732c78798c1944755befb7841
Luke Patterson <lsp52@cornell.edu>
2019-07-05 16:58:24 -0400

    u

--- MT/2_MT_matching.py
+++ MT/2_MT_matching.py
@@ -13,16 +13,14 @@ sos_df=pd.read_csv('C:/Users/lpatterson/AnacondaProjects/Tribal_Master/step_4_wo
     'MT/MT_candidate_records.csv')
 # drop a trademark picked up
 sos_df=sos_df.loc[sos_df['type']!='Service mark'].dropna(axis=1,how='all')
-import pdb; pdb.set_trace()
-# note type of each record
-sos_df['type'] = sos_df['Business ID'].str[0:2]
-# clean up line breaks in address columns
-addr_cols=['Agent Mailing Address','Mailing Address','Mailing Address2',
-    'Mailing Address3','Mailing Address4','Mailing Address5','Mailing Address6',
-    'Physical Address','Physical Address2','Physical Address3','Physical Address4',
-    'Physical Address5','Physical Address6','Principal Office']
-for i in addr_cols:
-    sos_df[i] = sos_df[i].str.replace('\r\n',' ')
+# can't match to assumed business names, which do not list entities. Can only match
+# with the corporations identified from the assumed business name entries
+sos_df=sos_df.loc[sos_df['type']!='Assumed Business Name'].dropna(axis=1,how='all')
+# need to recreate Business Identifier column, which didn't process properly
+sos_df['ID_idx_start']=sos_df['Record Name on Website'].str.find('(')
+sos_df['ID_idx_end']=sos_df['Record Name on Website'].str.find(')')
+sos_df['Business Identifier']=sos_df.apply(lambda x:
+        x['Record Name on Website'][x['ID_idx_start']+1:x['ID_idx_end']],axis=1)
 
 # load FDA data
 FDA_dir='C:/Users/lpatterson/AnacondaProjects/Tribal_Master'
@@ -45,7 +43,7 @@ for i,fda_row in fda_df.iterrows():
     temp_sos = sos_df.loc[sos_df['IMPAQ_ID'] == fda_row['IMPAQ_ID']]
     if temp_sos.empty == False:
         out = mf.name_match_scoring(fda_row, temp_sos, fda_colname='DBA Name_update',
-            sos_colname = 'Name', id_var='IMPAQ_ID')
+            sos_colname = 'Record Name on Website', id_var='IMPAQ_ID')
         # turns out address fails to predict anything
         # # try out all the different address columns for a match by address
         # addr_out = mf.address_match_scoring(fda_row, temp_sos, update=True,
@@ -67,60 +65,29 @@ for i,fda_row in fda_df.iterrows():
         if name_match.empty == False:
             match = temp_sos.loc[name_match.index,:]
             # if more than one tied, take the active record
-            if any(match['Status'].apply(lambda x: x in ['Active','Good Standing'])):
-                match = match.loc[(match['Status']=='Active') | (match['Status']=='Good Standing'),:]
+            if any(match['Status'].apply(lambda x: 'Active' in 'x')):
+                match = match.loc[('Active' in match['Status']),:]
                 name_match = name_match.loc[match.index,:]
             match = match.loc[name_match.name_score.idxmax(),:]
             # handling for corporation record
-            if match['type']=='DB':
-                fda_df.loc[i,'SoS_record'] = str(match['Business ID'])
-                fda_df.loc[i,'entity'] = match['Name']
-                fda_df.loc[i,'agent_name'] = match['Agent Name']
-                address = try_tag(match['Agent Address'])
-                if address != '':
-                    elems = ['AddressNumber', 'StreetNamePreType', 'StreetName',
-                    'StreetNamePostType', 'OccupancyType', 'OccupancyIdentifier']
-                    temp_elems=pd.Series(elems)
-                    for n,j in enumerate(elems):
-                        if j in address.keys():
-                            temp_elems.iloc[n]=True
-                        else:
-                            temp_elems.iloc[n]=False
-                    fda_df.loc[i,'agent_address'] = pd.Series(address).str.cat(
-                        na_rep='', sep = ' ').strip().upper()
-                    fda_df.loc[i,'agent_city'] = address['PlaceName']
-                    fda_df.loc[i,'agent_state'] = address['StateName']
-                    fda_df.loc[i,'agent_zip'] = str(address['ZipCode'])[0:5]
-            # handling for DBA record - company
-            if match['type']=='UB' and '- D' in match['Owner Name']:
-                fda_df.loc[i,'SoS_record'] = str(match['Business ID'])
-                trunc_idx = match['Owner Name'].find('- D')
-                fda_df.loc[i,'entity'] = match['Owner Name'][0:trunc_idx].strip()
-                fda_df.loc[i,'agent_name'] = 'Not found'
-                fda_df.loc[i,'agent_address'] = 'Not found'
-                fda_df.loc[i,'agent_city'] = 'Not found'
-                fda_df.loc[i,'agent_state'] = 'Not found'
-                fda_df.loc[i,'agent_zip'] = 'Not found'
-            # handling for DBA record - individual
-            if match['type']=='UB' and '- D' not in match['Owner Name']:
-                fda_df.loc[i,'SoS_record'] = str(match['Business ID'])
-                fda_df.loc[i,'entity'] = match['Owner Name'] + ' - Owned by an Individual'
-                fda_df.loc[i,'agent_name'] = match['Owner Name']
-                address = try_tag(match['Agent Address'])
-                if address != '':
-                    elems = ['AddressNumber', 'StreetNamePreType', 'StreetName',
-                    'StreetNamePostType', 'OccupancyType', 'OccupancyIdentifier']
-                    temp_elems=pd.Series(elems)
-                    for n,j in enumerate(elems):
-                        if j in address.keys():
-                            temp_elems.iloc[n]=True
-                        else:
-                            temp_elems.iloc[n]=False
-                    fda_df.loc[i,'agent_address'] = pd.Series(address).str.cat(
-                        na_rep='', sep = ' ').strip().upper()
-                    fda_df.loc[i,'agent_city'] = address['PlaceName']
-                    fda_df.loc[i,'agent_state'] = address['StateName']
-                    fda_df.loc[i,'agent_zip'] = str(address['ZipCode'])[0:5]
+            fda_df.loc[i,'SoS_record'] = str(match['Business Identifier'])
+            fda_df.loc[i,'entity'] = match['Record Name on Website']
+            fda_df.loc[i,'agent_name'] = match['Name']
+            address = try_tag(match['Postal Address'])
+            if address != '':
+                elems = ['AddressNumber', 'StreetNamePreType', 'StreetName',
+                'StreetNamePostType', 'OccupancyType', 'OccupancyIdentifier']
+                temp_elems=pd.Series(elems)
+                for n,j in enumerate(elems):
+                    if j in address.keys():
+                        temp_elems.iloc[n]=True
+                    else:
+                        temp_elems.iloc[n]=False
+                fda_df.loc[i,'agent_address'] = pd.Series(address).str.cat(
+                    na_rep='', sep = ' ').strip().upper()
+                fda_df.loc[i,'agent_city'] = address['PlaceName']
+                fda_df.loc[i,'agent_state'] = address['StateName']
+                fda_df.loc[i,'agent_zip'] = str(address['ZipCode'])[0:5]
             fda_df.loc[i,'URL'] = match['URL']
 fda_df.to_csv('C:/Users/lpatterson/AnacondaProjects/Tribal_Master/step_4_work/' +
     'MT/MT_FDA_matches.csv',index=False)
